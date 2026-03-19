fa1271e69ac59c038b234c3d72b635a727f51b3d
Wissarut Kanasub <114909150+Qosanglesz@users.noreply.github.com>
2026-03-19 07:26:09 +0000

    error predict detect

---
+++ run_training_serving_skew.py
@@ -0,0 +1,14 @@
+import joblib
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+
+prod = pd.read_csv("data/loan_production_skew.csv")
+
+X_prod = prod.drop("default", axis=1)
+y_prod = prod["default"]
+
+model = joblib.load("model.joblib")
+pred = model.predict(X_prod)
+
+print("Production accuracy:", accuracy_score(y_prod, pred))
